042598dfd27039d9ca2942b12925b2a1e8d60f9b
liyinxiang <liyxzsu@gmail.com>
2019-01-10 16:09:02 +0800

    disable openvpn server mod

--- install.sh
+++ install.sh
@@ -42,23 +42,6 @@ fi
 
 base_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-
-printf "\n################## Server informations ##################\n"
-
-read -p "Server Hostname/IP: " ip_server
-
-read -p "OpenVPN protocol (tcp or udp) [tcp]: " openvpn_proto
-
-if [[ -z $openvpn_proto ]]; then
-  openvpn_proto="tcp"
-fi
-
-read -p "Port [443]: " server_port
-
-if [[ -z $server_port ]]; then
-  server_port="443"
-fi
-
 # Get root pass (to create the database and the user)
 mysql_root_pass=""
 status_code=1
@@ -91,130 +74,6 @@ read -p "MySQL user password for OpenVPN-Admin: " -s mysql_pass; echo
 # TODO MySQL port & host ?
 
 
-printf "\n################## Certificates informations ##################\n"
-
-read -p "Key size (1024, 2048 or 4096) [2048]: " key_size
-
-read -p "Root certificate expiration (in days) [3650]: " ca_expire
-
-read -p "Certificate expiration (in days) [3650]: " cert_expire
-
-read -p "Country Name (2 letter code) [US]: " cert_country
-
-read -p "State or Province Name (full name) [California]: " cert_province
-
-read -p "Locality Name (eg, city) [San Francisco]: " cert_city
-
-read -p "Organization Name (eg, company) [Copyleft Certificate Co]: " cert_org
-
-read -p "Organizational Unit Name (eg, section) [My Organizational Unit]: " cert_ou
-
-read -p "Email Address [me@example.net]: " cert_email
-
-read -p "Common Name (eg, your name or your server's hostname) [ChangeMe]: " key_cn
-
-
-printf "\n################## Creating the certificates ##################\n"
-
-EASYRSA_RELEASES=( $(
-  curl -s https://api.github.com/repos/OpenVPN/easy-rsa/releases | \
-  grep 'tag_name' | \
-  grep -E '3(\.[0-9]+)+' | \
-  awk '{ print $2 }' | \
-  sed 's/[,|"|v]//g'
-) )
-
-EASYRSA_LATEST=${EASYRSA_RELEASES[0]}
-
-if [[ ! -z $EASYRSA_LATEST ]]; then
-  EASYRSA_LATEST=3.0.5
-fi
-
-# Get the rsa keys
-wget -q https://github.com/OpenVPN/easy-rsa/releases/download/v${EASYRSA_LATEST}/EasyRSA-nix-${EASYRSA_LATEST}.tgz
-tar -xaf EasyRSA-nix-${EASYRSA_LATEST}.tgz
-mv EasyRSA-${EASYRSA_LATEST} /etc/openvpn/easy-rsa
-rm -r EasyRSA-nix-${EASYRSA_LATEST}.tgz
-cd /etc/openvpn/easy-rsa
-
-if [[ ! -z $key_size ]]; then
-  export EASYRSA_KEY_SIZE=$key_size
-fi
-if [[ ! -z $ca_expire ]]; then
-  export EASYRSA_CA_EXPIRE=$ca_expire
-fi
-if [[ ! -z $cert_expire ]]; then
-  export EASYRSA_CERT_EXPIRE=$cert_expire
-fi
-if [[ ! -z $cert_country ]]; then
-  export EASYRSA_REQ_COUNTRY=$cert_country
-fi
-if [[ ! -z $cert_province ]]; then
-  export EASYRSA_REQ_PROVINCE=$cert_province
-fi
-if [[ ! -z $cert_city ]]; then
-  export EASYRSA_REQ_CITY=$cert_city
-fi
-if [[ ! -z $cert_org ]]; then
-  export EASYRSA_REQ_ORG=$cert_org
-fi
-if [[ ! -z $cert_ou ]]; then
-  export EASYRSA_REQ_OU=$cert_ou
-fi
-if [[ ! -z $cert_email ]]; then
-  export EASYRSA_REQ_EMAIL=$cert_email
-fi
-if [[ ! -z $key_cn ]]; then
-  export EASYRSA_REQ_CN=$key_cn
-fi
-
-# Init PKI dirs and build CA certs
-./easyrsa init-pki
-./easyrsa build-ca nopass
-# Generate Diffie-Hellman parameters
-./easyrsa gen-dh
-# Genrate server keypair
-./easyrsa build-server-full server nopass
-
-# Generate shared-secret for TLS Authentication
-openvpn --genkey --secret pki/ta.key
-
-
-printf "\n################## Setup OpenVPN ##################\n"
-
-# Copy certificates and the server configuration in the openvpn directory
-cp /etc/openvpn/easy-rsa/pki/{ca.crt,ta.key,issued/server.crt,private/server.key,dh.pem} "/etc/openvpn/"
-cp "$base_path/installation/server.conf" "/etc/openvpn/"
-mkdir "/etc/openvpn/ccd"
-sed -i "s/port 443/port $server_port/" "/etc/openvpn/server.conf"
-
-if [ $openvpn_proto = "udp" ]; then
-  sed -i "s/proto tcp/proto $openvpn_proto/" "/etc/openvpn/server.conf"
-fi
-
-nobody_group=$(id -ng nobody)
-sed -i "s/group nogroup/group $nobody_group/" "/etc/openvpn/server.conf"
-
-printf "\n################## Setup firewall ##################\n"
-
-# Make ip forwading and make it persistent
-echo 1 > "/proc/sys/net/ipv4/ip_forward"
-echo "net.ipv4.ip_forward = 1" >> "/etc/sysctl.conf"
-
-# Get primary NIC device name
-primary_nic=`route | grep '^default' | grep -o '[^ ]*$'`
-
-# Iptable rules
-iptables -I FORWARD -i tun0 -j ACCEPT
-iptables -I FORWARD -o tun0 -j ACCEPT
-iptables -I OUTPUT -o tun0 -j ACCEPT
-
-iptables -A FORWARD -i tun0 -o $primary_nic -j ACCEPT
-iptables -t nat -A POSTROUTING -o $primary_nic -j MASQUERADE
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o $primary_nic -j MASQUERADE
-iptables -t nat -A POSTROUTING -s 10.8.0.2/24 -o $primary_nic -j MASQUERADE
-
-
 printf "\n################## Setup MySQL database ##################\n"
 
 echo "CREATE DATABASE \`openvpn-admin\`" | mysql -u root --password="$mysql_root_pass"
@@ -244,18 +103,9 @@ cd "$openvpn_admin"
 sed -i "s/\$user = '';/\$user = '$mysql_user';/" "./include/config.php"
 sed -i "s/\$pass = '';/\$pass = '$mysql_pass';/" "./include/config.php"
 
-# Replace in the client configurations with the ip of the server and openvpn protocol
-for file in "./client-conf/gnu-linux/client.conf" "./client-conf/osx-viscosity/client.conf" "./client-conf/windows/client.ovpn"; do
-  sed -i "s/remote xxx\.xxx\.xxx\.xxx 443/remote $ip_server $server_port/" $file
-
-  if [ $openvpn_proto = "udp" ]; then
-    sed -i "s/proto tcp-client/proto udp/" $file
-  fi
-done
-
 # Copy ta.key inside the client-conf directory
 for directory in "./client-conf/gnu-linux/" "./client-conf/osx-viscosity/" "./client-conf/windows/"; do
-  cp "/etc/openvpn/"{ca.crt,ta.key} $directory
+  cp "/etc/openvpn/keys/"{ca.crt,ta.key} $directory
 done
 
 # Install third parties
